042a3072fb47d9dd2d4e2ae14b002384bdfd3a3f
T.Y.M.SAI <tymsai6076@gmail.com>
2021-06-04 22:41:11 +0530

    Update Opening_Closing.cpp

--- Opening_Closing.cpp
+++ Opening_Closing.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main (){
     fstream xyz;
-    xyz.open("xyz",ios::out);
+    xyz.open("abc",ios::out);
     cout<<"created xyz named file";
     xyz.close();
     return 0;
